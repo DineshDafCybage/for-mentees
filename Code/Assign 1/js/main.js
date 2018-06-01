3635fa1aeba496ee90b5f192786dff60520a9adf
DineshDafCybage <dineshda@cybage.com>
2018-06-01 18:19:41 +0530

    first commit

---
+++ Code/Assign 1/js/main.js
@@ -0,0 +1,57 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+(function() {
+
+    var hamburger = {
+        navToggle: document.querySelector('.nav-toggle'),
+        nav: document.querySelector('nav'),
+
+        doToggle: function(e) {
+            e.preventDefault();
+            this.navToggle.classList.toggle('expanded');
+            this.nav.classList.toggle('expanded');
+        }
+    };
+
+    hamburger.navToggle.addEventListener('click', function(e) {
+        hamburger.doToggle(e);
+    });
+    hamburger.nav.addEventListener('click', function(e) {
+        hamburger.doToggle(e);
+    });
+
+}());

286b3287f3d4d9016e86290cb495e70efaec5073
DineshDafCybage <dineshda@cybage.com>
2018-06-08 20:05:14 +0530

    latest patch 6/8

--- Project/js/script.js
+++ Project/js/script.js
@@ -6,22 +6,47 @@
   };
 
  function init(){
+  var form = document.getElementById("form");
+  var scrollbtn = document.querySelector('.scroll');
+
   //Show/hide navbar on click of menu-icon
   menubtn.addEventListener('click', toggleMenu);
+
+  //check form field on sumit of form
+  form.addEventListener('submit', checkInput);
+
+  //scroll down on click of scroll down icon
+  scrollbtn.addEventListener('click', scrollDown);
  }
 
 
+function checkInput() {
+  var email = document.forms["form"]["email"].value;
+  var atpos = email.indexOf("@");
+  var dotpos = email.lastIndexOf(".");
+  if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+    alert("Not a valid e-mail address");
+    return false;
+  }
+}
+ 
+
 function toggleMenu() {
-    
+
   var navbar = document.getElementById("myTopnav");
   var menubtn = document.getElementById("menubtn");
 
     if (navbar.className === "show") {
-        navbar.className = "close";
-        menubtn.className = "menu-icon";
+      navbar.className = "close";
+      menubtn.className = "menu-icon";
     } else {
         navbar.className = "show";
         menubtn.className = "close-icon";
     }
 }
 
+function scrollDown() {
+  document.querySelector('#our-drones').scrollIntoView({
+  behavior: 'smooth'
+});
+}

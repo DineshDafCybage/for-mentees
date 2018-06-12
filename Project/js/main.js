5177543397e9d41514a44ed8cd613f7b55ea3038
DineshDafCybage <dineshda@cybage.com>
2018-06-12 20:55:49 +0530

    6/12

---
+++ Project/js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+window.onload = initialize;
+
+function initialize() {
+  document.getElementById("hamburger-btn").addEventListener('click', fnToggleMenu);
+}
+
+/* Code to handle Hamburger */
+function fnToggleMenu() {
+var ele = document.getElementById("hamburger-btn");
+var eleNav = document.getElementById('nav-bar');
+ if (ele.classList.contains('show-menu')) {
+      ele.classList.remove('show-menu');
+      ele.classList.add('hide-menu');
+      document.getElementsByClassName('menuBtn')[0].setAttribute("src","images/close-icon.png");
+
+      /* Code for showing Menu Items on click of Hamburger */
+      if(eleNav.classList.contains('hide-nav')) { 
+        eleNav.classList.add('show-nav');
+        eleNav.classList.remove('hide-nav');
+       }
+    }
+    else {  
+      document.getElementsByClassName('menuBtn')[0].setAttribute("src","images/menu-icon.png");
+      ele.classList.remove('hide-menu');
+      ele.classList.add('show-menu');
+      
+      /* Condition for hiding Menu Items on click of Hamburger */
+       if (eleNav.classList.contains('show-nav')) {
+        eleNav.classList.remove('show-nav');
+        eleNav.classList.add('hide-nav');
+       }
+    }
+}
